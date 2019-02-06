efad151659ee1a033f57f77af133fe2c0dec214d
Jitendra Singh <jitendramalan7@gmail.com>
2019-02-06 07:47:49 +0530

    added ngrx store and created a reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,8 +11,11 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
 
 @NgModule({
+
+
   declarations: [
     AppComponent,
   ],
@@ -20,7 +26,8 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,22 @@
+import * as Actions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+
+const initialState = {
+  ingredients: [new Ingredient('Apples', 100), new Ingredient('Tomatos', 20)]
+};
+
+export function shoppingListReducer(
+  state = initialState,
+  action: Actions.ShoppingListActions
+) {
+  switch (action.type) {
+    case Actions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
+  }
+}
